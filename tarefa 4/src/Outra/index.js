7584f590681415c929da69ad4b30931fb5ae8ead
mahzze <zanzinzzo@gmail.com>
2022-12-02 09:13:37 -0300

    finalizada a ultima tarefa

--- tarefa 4/src/Outra/index.js
+++ tarefa 4/src/Outra/index.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
   const navigation = useNavigation();
+  const style = StyleSheet.create({
+    body: {
+      backgroundColor: "#000",
+      minHeight: "60vh",
+      display: 'flex',
+      flexDirection: 'column'
+    }
+
+  })
+
   return (
-    <View>
-      <Text> FAZER UM PLANO DE FUNDO PRETO COM A IMAGEM DA NETFLIX AQUI NO MEIO DA AULA </Text>
+    <View style={style.body}>
+      <Image source={ require('./logo.png') } style={{ width:'100%', height:'20%'}} />
+      <Text style={{color: "#ffeedd"}}>Teste</Text>
       <Button
         title="Ir para Home"
         onPress={() => navigation.navigate('Home')}
       />
-      <View >
-      </View>
+      
     </View>
   );
 }
